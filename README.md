8668bab8ae33259e30731f6e927adee794f7f2e0
ygorportes <ygorportes3@gmail.com>
2024-06-06 22:33:40 -0300

    Criação do projeto

---
+++ README.md
@@ -0,0 +1,3 @@
+## Conversor de temperatura - Celsius para Fahrenheit
+
+Conversor de Celsius para Fahrenheit que pede uma temperatura em graus Celsius e, em seguida, converte-a para Fahrenheit.
---
+++ src/CelsiusToFahrenheit.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class CelsiusToFahrenheit {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite a temperatura em Celsius");
+        int celsius = scanner.nextInt();
+        scanner.close();
+
+        int fahrenheit = (celsius * 9/5) + 32;
+        System.out.println(celsius + "º Celsius é igual a " + fahrenheit + "º Fahrenheit.");
+    }
+}
